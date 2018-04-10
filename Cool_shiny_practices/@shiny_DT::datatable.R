71b4822de48c495da9c9a472071810526f883f50
chaudharyparth <parthc3233@gmail.com>
2018-04-10 17:15:26 +0530

    add link within datatable

--- Cool_shiny_practices/@shiny_DT::datatable.R
+++ Cool_shiny_practices/@shiny_DT::datatable.R
@@ -1,2 +1,37 @@
 # datatable customization
 https://rstudio.github.io/DT/options.html
+
+
+# add link within datatable
+library("shiny")
+library("shinydashboard")
+library("datasets")
+library("DT")
+library("shinyBS")
+
+header <- dashboardHeader()
+
+sidebar <- dashboardSidebar()
+
+body <- dashboardBody(
+  DT::dataTableOutput("mtcarsTable"),
+  bsModal("mtCarsModal", "My Modal", "",tags$p("Hello World"), size = "small")
+)
+
+shinyApp(
+  ui = dashboardPage(header, sidebar, body),
+  server = function(input, output, session) {
+    
+    mtcarsLinked <- reactive({   
+      mtcars$mpg <- sapply(datasets::mtcars$mpg, function(x) {as.character(tags$a(href = "#", onclick = "$('#mtCarsModal').modal('show')", x))})
+      return(mtcars)
+    })
+    
+    output$mtcarsTable <- DT::renderDataTable({
+      DT::datatable(mtcarsLinked(), 
+                    class = 'compact',
+                    escape = FALSE
+      )
+    })
+  }
+)
